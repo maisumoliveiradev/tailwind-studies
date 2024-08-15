44f19b8d415299929d40186390c969601329b965
ViniciusdeAOliveira <48970710+maisumoliveiradev@users.noreply.github.com>
2024-08-15 20:27:26 -0300

    Aula 2 - Tailwind CSS - Estados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-6xl">Esté é um H1</h1>
+      <h1 className="font-bold text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">Esté é um H1</h1>
       <h2>Esté é um H2</h2>
       <p>Esté é um p</p>
+      <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
+        Sign in
+      </button>
 
     </div>
   );
