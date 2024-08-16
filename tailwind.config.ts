6cbc490f1488a12144e8213a0b7ed44638dc34f9
ViniciusdeAOliveira <48970710+maisumoliveiradev@users.noreply.github.com>
2024-08-15 21:08:23 -0300

    Estrutura do Layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/**/*.tsx",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
